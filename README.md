5140ac0052174784630529e1962749b892e80c38
Uğur Gürbüz <93009859+ugur-gurbuz@users.noreply.github.com>
2021-12-14 17:28:16 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kişisel Web Sitesi
+
+Uğur Gürbüz | Front-End Developer
+
+Kişisel Web Sitesi Çalışması
+
+Herhangi bir framework kullanılmamaya özen gösterilmiştir
+
+© 2022 Uğur Gürbüz Tüm Hakları Saklıdır
---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/* Menu Show */ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/* Active and Remove Menu */
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /* Active link */
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /* Remove menu mobile */
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* Scroll Reveal Animation */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/* Ana Sayfa Scroll */
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/* Hakkımda Scroll */
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/* Yetenekler Scroll */
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/* Projeler Scroll */
+sr.reveal('.work__img',{interval: 200}); 
+
+/* İletişim Scroll */
+sr.reveal('.contact__input',{interval: 200});
